8db6590d9e6de3d228c7f7d3a41052ce76fb5346
Elijah Hezekiah <elijah.hezekiah@contractor.brightinsight.com>
2023-09-09 19:42:47 +0100

    update todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import React, { state,setState } from 'react';
+import React, { useState } from 'react';
+import Todos from './Todos'
 
 function App() {
+  const [ state, setState ] = useState( [
+    { id:1, content: 'buy some milk'},
+    { id:2, content: 'play mario kart'}
+  ]
+  );
+  // state = {
+  //   todos :[ 
+  //     { id:1, content: 'buy some milk'},
+  //     { id:2, content: 'play mario kart'}
+  //   ]
+  // }
+  const deleteTodo = (id) => {
+    //console.log(id);
+   
+    setState(
+     state.filter(todo => 
+       todo.id !== id
+      )
+    ) 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <h1 className="center blue-text">Todo's</h1>
+       <Todos todos={state} deleteTodo={deleteTodo}/>
+     
     </div>
   );
 }
